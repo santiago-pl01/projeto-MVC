863feb8413b3e30b5ca4abf84bbd9ff84aa7dd86
nicolecs9 <nnaiara585@gmail.com>
2025-04-10 16:34:30 -0300

    primeiro commit de controller

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from controlles import cadastrar_usuario
 
 app= Blueprint('app', __name__)
 
@@ -6,3 +7,16 @@ app= Blueprint('app', __name__)
 def home():
     return render_template('index.html')
     
+@app.route('/cadastro', methods=['GET', 'POST'])
+
+def cadastro():
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+
+        mensagem = cadastrar_usuario(nome, email, senha)
+        return render_template ('cadastro.html',mensagem=mensagem)
+    
+    return render_template('cadastro.html')
+
